c6db1f5090ca0a2bb924e7b198a9f0e281c43d13
Nicholai <nicholaivogelfilms@gmail.com>
2026-02-04 23:17:38 -0700

    feat: install nvim-autopairs with treesitter integration

--- lua/plugins/plugin.lua
+++ lua/plugins/plugin.lua
@@ -160,6 +160,27 @@ return {
         end,
     },
 
+    -- Auto-pairs
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {
+            check_ts = true,
+            ts_config = {
+                lua = { "string", "source" },
+                javascript = { "string", "template_string" },
+                typescript = { "string", "template_string" },
+            },
+            disable_filetype = { "TelescopePrompt", "spectre_panel" },
+        },
+        config = function(_, opts)
+            require("nvim-autopairs").setup(opts)
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            local cmp = require("cmp")
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+    },
+
     -- LSP
     {
         "williamboman/mason.nvim",
